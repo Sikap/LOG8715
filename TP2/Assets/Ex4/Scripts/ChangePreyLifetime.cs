e37dbaf0f6ef8f630ba61130401a66a4f324c019
Simon Kaplo <simon.kaplo@polymtl.ca>
2023-03-19 23:31:06 -0400

    FINAL COMMIT

--- TP2/Assets/Ex4/Scripts/ChangePreyLifetime.cs
+++ TP2/Assets/Ex4/Scripts/ChangePreyLifetime.cs
@@ -11,11 +11,7 @@ using ParallelJobs;
 public class ChangePreyLifetime : MonoBehaviour
 {
     private Lifetime _lifetime;
-    private NativeArray<float3> possitionArray;
-    private NativeArray<float> lifeTimeDecreasingFactorArray;
-    private NativeArray<bool> lifeTimeReproducedArray;
 
-    private TransformAccessArray transformAccessArray;
     public void Start()
     {
         _lifetime = GetComponent<Lifetime>();
@@ -23,7 +19,7 @@ public class ChangePreyLifetime : MonoBehaviour
 
     public void Update()
     {
-        /*_lifetime.decreasingFactor = 1.0f;
+        _lifetime.decreasingFactor = 1.0f;
         foreach(var plant in Ex4Spawner.PlantTransforms)
         {
             if (Vector3.Distance(plant.position, transform.position) < Ex4Config.TouchingDistance)
@@ -49,86 +45,6 @@ public class ChangePreyLifetime : MonoBehaviour
                 _lifetime.reproduced = true;
                 break;
             }
-        }*/
-        possitionArray = new NativeArray<float3>(Ex4Spawner.PlantTransforms.Length,Allocator.TempJob);
-        transformAccessArray = new TransformAccessArray(Ex4Spawner.PlantTransforms.Length);
-        lifeTimeDecreasingFactorArray = new NativeArray<float>(Ex4Spawner.PlantTransforms.Length,Allocator.TempJob);
-        for(int i = 0 ;i<Ex4Spawner.PlantTransforms.Length;i++)
-        {
-            possitionArray[i] = Ex4Spawner.PlantTransforms[i].position;
-            transformAccessArray.Add(transform);
         }
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            lifeTimeDecreasingFactorArray[i] =  1.0f;
-        }
-        DivideLifeTimeDecreasingFactor dividePreyLifetimeParallelJob = new DivideLifeTimeDecreasingFactor
-        {
-            touchingDistance =  Ex4Config.TouchingDistance,
-            possitionArray = possitionArray,
-            lifeTimeDecreasingFactorArray = lifeTimeDecreasingFactorArray,
-        };
-        JobHandle jobPrayLifetimeHandle = dividePreyLifetimeParallelJob.Schedule(transformAccessArray);
-        jobPrayLifetimeHandle.Complete();
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            Ex4Spawner.PreyLifetimes[i].decreasingFactor = lifeTimeDecreasingFactorArray[i];   
-        }
-        possitionArray.Dispose();
-        transformAccessArray.Dispose();
-        lifeTimeDecreasingFactorArray.Dispose();
-
-
-        possitionArray = new NativeArray<float3>(Ex4Spawner.PredatorTransforms.Length,Allocator.TempJob);        
-        transformAccessArray = new TransformAccessArray(Ex4Spawner.PredatorTransforms.Length);        
-        lifeTimeDecreasingFactorArray =  new NativeArray<float>(Ex4Spawner.PreyLifetimes.Length,Allocator.TempJob);
-        for(int i = 0 ;i<Ex4Spawner.PredatorTransforms.Length;i++)
-        {
-            possitionArray[i] = Ex4Spawner.PredatorTransforms[i].position;
-            transformAccessArray.Add(transform);
-        }
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            lifeTimeDecreasingFactorArray[i] = 1.0f;            
-        }
-        MultiplyLifetimeDeacreasingFactor multiplyPreyLifetimeParallelJob = new MultiplyLifetimeDeacreasingFactor{
-            touchingDistance =  Ex4Config.TouchingDistance,
-            possitionArray = possitionArray,
-            lifeTimeDecreasingFactorArray = lifeTimeDecreasingFactorArray,
-        };
-        JobHandle jobHandle = multiplyPreyLifetimeParallelJob.Schedule(transformAccessArray);
-        jobHandle.Complete();
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            Ex4Spawner.PreyLifetimes[i].decreasingFactor = lifeTimeDecreasingFactorArray[i];   
-        }
-        possitionArray.Dispose();
-        transformAccessArray.Dispose();
-        lifeTimeDecreasingFactorArray.Dispose();
-
-
-        possitionArray = new NativeArray<float3>(Ex4Spawner.PreyTransforms.Length,Allocator.TempJob);
-        transformAccessArray = new TransformAccessArray(Ex4Spawner.PreyTransforms.Length);
-        lifeTimeReproducedArray = new NativeArray<bool>(Ex4Spawner.PreyLifetimes.Length,Allocator.TempJob);
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            possitionArray[i] = Ex4Spawner.PreyTransforms[i].position;
-            lifeTimeReproducedArray[i] = Ex4Spawner.PreyLifetimes[i].reproduced;
-            transformAccessArray.Add(transform);
-        }
-        ChangeLifetimeReproduction changeLifetimeReproduction = new ChangeLifetimeReproduction{
-            touchingDistance =  Ex4Config.TouchingDistance,
-            possitionArray = possitionArray,
-            lifeTimeReproducedArray =  lifeTimeReproducedArray,
-        };
-        JobHandle jobPredatorLifetimeHandle = changeLifetimeReproduction.Schedule(transformAccessArray);
-        jobPredatorLifetimeHandle.Complete();       
-        for(int i = 0 ;i<Ex4Spawner.PreyLifetimes.Length;i++)
-        {
-            Ex4Spawner.PreyLifetimes[i].reproduced = lifeTimeReproducedArray[i];   
-        }
-        possitionArray.Dispose();
-        transformAccessArray.Dispose();
-        lifeTimeReproducedArray.Dispose();
     }
 }
